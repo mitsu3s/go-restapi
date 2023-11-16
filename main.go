2ae5bbe6f93f7309a5e5c6625cbdeb37751802ec
mitsu3s <mitsuuu.s3s@gmail.com>
2023-11-16 18:17:58 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	log.Println("Starting server...")
+	router := gin.Default()
+
+	// ルーティングの設定
+	router.GET("/hello", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+	// エラーが発生した場合にログを出力する
+	log.Fatal(router.Run(":8000"))
+}
